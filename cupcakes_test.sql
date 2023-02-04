f51d267d34b69139266b30d883745f3ee4a4cebc
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-03 21:05:48 -0800

    initial commit

---
+++ cupcakes_test.sql
@@ -0,0 +1,23 @@
+-- from the terminal run:
+-- psql < cupcake.sql
+
+DROP DATABASE IF EXISTS cupcake;
+
+CREATE DATABASE cupcake;
+
+\c cupcake
+
+CREATE TABLE cupcakes
+(
+    id SERIAL PRIMARY KEY,
+    flavor TEXT NOT NULL,
+    size TEXT NOT NULL,
+    rating DECIMAL NOT NULL,
+    image TEXT NOT NULL DEFAULT 'https://tinyurl.com/demo-cupcake'
+);
+
+INSERT INTO cupcakes ( flavor, size, rating, image )
+VALUES
+('chocolate', 'sm', 3.5, 'https://www.google.com/aclk?sa=l&ai=DChcSEwjFn_LCifv8AhUFJq0GHSdrBTsYABABGgJwdg&sig=AOD64_1KhRTOMfeCnoT3Ug0JVlWT5Pm9vg&adurl&ctype=5&ved=2ahUKEwjxl-PCifv8AhVYB0QIHR3JDnwQvhd6BAgBEG8'),
+('vanilla', 'md', 2.5, 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSceX1jlTG8Jz4ePeqoFkvzOg5CSj-xAX32Be-ravkgSHD37JkSCZP0b5g07MY&usqp=CAE'),
+('blueberry', 'lg', 1.0, 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT1UiB8LUuwJFZKR_TF7i-cJLX4_BnKsusAHAkoGLmGfQemHBs&usqp=CAE');
