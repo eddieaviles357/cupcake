8382c37dfb70510ab7f43583f7f5c5afd662fa22
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-05 13:29:41 -0800

    changed image url

--- cupcakes_test.sql
+++ cupcakes_test.sql
@@ -18,6 +18,6 @@ CREATE TABLE cupcakes
 
 INSERT INTO cupcakes ( flavor, size, rating, image )
 VALUES
-('chocolate', 'sm', 3.5, 'https://www.google.com/aclk?sa=l&ai=DChcSEwjFn_LCifv8AhUFJq0GHSdrBTsYABABGgJwdg&sig=AOD64_1KhRTOMfeCnoT3Ug0JVlWT5Pm9vg&adurl&ctype=5&ved=2ahUKEwjxl-PCifv8AhVYB0QIHR3JDnwQvhd6BAgBEG8'),
+('chocolate', 'sm', 3.5, 'https://dinnerthendessert.com/wp-content/uploads/2021/02/Chocolate-Cupcakes-1x1-1.jpg'),
 ('vanilla', 'md', 2.5, 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSceX1jlTG8Jz4ePeqoFkvzOg5CSj-xAX32Be-ravkgSHD37JkSCZP0b5g07MY&usqp=CAE'),
 ('blueberry', 'lg', 1.0, 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcT1UiB8LUuwJFZKR_TF7i-cJLX4_BnKsusAHAkoGLmGfQemHBs&usqp=CAE');
